cfc1786e0e5e24bcc732778437b30d5b13252f0e
Adrien Devresse <adrien.devresse@epfl.ch>
2016-12-06 13:32:50 +0100

    Improve BGQ portability Update mpi-cpp dependency Resolve Boost CMake configuration issue

--- CMake/portability/BlueGenePortability.cmake
+++ CMake/portability/BlueGenePortability.cmake
@@ -8,23 +8,29 @@ if(IS_DIRECTORY "/bgsys" AND (NOT DEFINED BLUEGENE) )
 endif()
 
 
-if(BLUEGENE AND CMAKE_C_COMPILER_IS_XLC )
+if(BLUEGENE)
+
+if(CMAKE_C_COMPILER_IS_XLC)
 	# define library type to static on BGQ
 	set(COMPILE_LIBRARY_TYPE "STATIC")
+
+	## static linking need to be forced on BlueGene
+	# Boost need a bit of tuning parameters for static linking
+	set(Boost_USE_STATIC_LIBS TRUE)	
+
+	#enforce static linking for hdf5
+	set(HDF5_USE_STATIC_LIBRARIES TRUE)
+endif()
+
+
 	## Blue Gene/Q do not support linking with MPI library when compiled with mpicc wrapper
         ## we disable any MPI_X_LIBRARY linking and rely on mpicc wrapper
 	set(MPI_LIBRARIES "")
 	set(MPI_C_LIBRARIES "")	
 	set(MPI_CXX_LIBRARIES "")
 
-	## static linking need to be forced on BlueGene
-	# Boost need a bit of tuning parameters for static linking
+
 	set(Boost_NO_BOOST_CMAKE TRUE)
-	set(Boost_USE_STATIC_LIBS TRUE)	
-
-	#enforce static linking for hdf5
-	set(HDF5_USE_STATIC_LIBRARIES TRUE)
-
 else()
 
 if(NOT DEFINED COMPILE_LIBRARY_TYPE)
